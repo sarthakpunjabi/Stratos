1de9f5af8ad83791e4f6f01e2f6d7a999f086fc5
KOMAL GANESH IYER <21025398@studentmail.ul.ie>
2022-04-27 16:12:15 +0100

    Results-Modeule-Pylint-changes

--- stratos/results/apps.py
+++ stratos/results/apps.py
@@ -1,6 +1,12 @@
+'''
+        Results App
+    '''
 from django.apps import AppConfig
 
 
 class ResultsConfig(AppConfig):
+    '''
+        Results App
+    '''
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'results'
