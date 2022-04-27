f6a9f5174814856d0e47648ffab1bf4c5cc58808
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-04-27 11:48:45 +0100

    Adding factory

--- stratos/quizes/factory.py
+++ stratos/quizes/factory.py
@@ -1,157 +1,102 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
-
-
-class AbstractFactory(ABC):
-    """
-    The Abstract Factory interface declares a set of methods that return
-    different abstract products. These products are called a family and are
-    related by a high-level theme or concept. Products of one family are usually
-    able to collaborate among themselves. A family of products may have several
-    variants, but the products of one variant are incompatible with products of
-    another.
-    """
-    @abstractmethod
-    def create_product_a(self) -> AbstractProductA:
-        pass
-
-    @abstractmethod
-    def create_product_b(self) -> AbstractProductB:
-        pass
-
-
-class ConcreteFactory1(AbstractFactory):
-    """
-    Concrete Factories produce a family of products that belong to a single
-    variant. The factory guarantees that resulting products are compatible. Note
-    that signatures of the Concrete Factory's methods return an abstract
-    product, while inside the method a concrete product is instantiated.
-    """
-
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA1()
-
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB1()
-
-
-class ConcreteFactory2(AbstractFactory):
-    """
-    Each Concrete Factory has a corresponding product variant.
-    """
-
-    def create_product_a(self) -> AbstractProductA:
-        return ConcreteProductA2()
-
-    def create_product_b(self) -> AbstractProductB:
-        return ConcreteProductB2()
-
-
-class AbstractProductA(ABC):
-    """
-    Each distinct product of a product family should have a base interface. All
-    variants of the product must implement this interface.
-    """
-
-    @abstractmethod
-    def useful_function_a(self) -> str:
-        pass
-
-
 """
-Concrete Products are created by corresponding Concrete Factories.
+This implements the factory interface for Rentezzy Application.
 """
+from . import state_pattern, validator, payment_modes
 
 
-class ConcreteProductA1(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A1."
-
-
-class ConcreteProductA2(AbstractProductA):
-    def useful_function_a(self) -> str:
-        return "The result of the product A2."
-
-
-class AbstractProductB(ABC):
+class PaymentFactory:
     """
-    Here's the the base interface of another product. All products can interact
-    with each other, but proper interaction is possible only between products of
-    the same concrete variant.
+    Factory class for determining payment methods.
     """
-    @abstractmethod
-    def useful_function_b(self) -> None:
-        """
-        Product B is able to do its own thing...
-        """
+    def __init__(self):
         pass
 
-    @abstractmethod
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> None:
+    @staticmethod
+    def choose_operator(payment_obj):
         """
-        ...but it also can collaborate with the ProductA.
+        Method for selecting the concrete function for payment.
+        """
+        target_class = payment_obj.get("type") + "Mode"
+        return getattr(payment_modes, target_class)(payment_obj)
 
-        The Abstract Factory makes sure that all products it creates are of the
-        same variant and thus, compatible.
-        """
+
+class ValidatorFactory:
+    """
+    Factory class for determining Validation methods.
+    """
+    def __init__(self):
         pass
 
-
-"""
-Concrete Products are created by corresponding Concrete Factories.
-"""
-
-
-class ConcreteProductB1(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B1."
-
-    """
-    The variant, Product B1, is only able to work correctly with the variant,
-    Product A1. Nevertheless, it accepts any instance of AbstractProductA as an
-    argument.
-    """
-
-    def another_useful_function_b(self, collaborator: AbstractProductA) -> str:
-        result = collaborator.useful_function_a()
-        return f"The result of the B1 collaborating with the ({result})"
-
-
-class ConcreteProductB2(AbstractProductB):
-    def useful_function_b(self) -> str:
-        return "The result of the product B2."
-
-    def another_useful_function_b(self, collaborator: AbstractProductA):
+    @staticmethod
+    def validate(request_details):
         """
-        The variant, Product B2, is only able to work correctly with the
-        variant, Product A2. Nevertheless, it accepts any instance of
-        AbstractProductA as an argument.
+        Method for selecting the concrete function for validating the request data.
         """
-        result = collaborator.useful_function_a()
-        return f"The result of the B2 collaborating with the ({result})"
+        method_list = [
+            method
+            for method in dir(validator.Validators)
+            if method.endswith('_validation') is True
+        ]
+        for method in method_list:
+            validate = validator.Validators()
+            return getattr(validate, method)(request_details)
+        return True
+
+    @staticmethod
+    def validate_cancel_booking(room_id):
+        """
+        Method for selecting the concrete function for validating request data.
+        """
+        method_list = [
+            method
+            for method in dir(validator.CancelBookingValidator)
+            if method.endswith('_validation') is True
+        ]
+        for method in method_list:
+            validate = validator.CancelBookingValidator()
+            return getattr(validate, method)(room_id)
+        return True
 
 
-def client_code(factory: AbstractFactory) -> None:
+class AgentCommissionFactory:
     """
-    The client code works with factories and products only through abstract
-    types: AbstractFactory and AbstractProduct. This lets you pass any factory
-    or product subclass to the client code without breaking it.
+    Factory class for calculating Agent commission.
     """
-    product_a = factory.create_product_a()
-    product_b = factory.create_product_b()
+    def __init__(self):
+        pass
 
-    print(f"{product_b.useful_function_b()}")
-    print(f"{product_b.another_useful_function_b(product_a)}", end="")
+    @staticmethod
+    def calculate_commission(agent_level, rooms):
+        """
+        Method for selecting the concrete function for calculating commission.
+        """
+        target_state = agent_level.title() + "State"
+        # Check if the agent is a Platinum Agent depending on number of room booked
+        if agent_level != "Platinum" and len(rooms) > 10:
+            target_state = "PlatinumState"
+        agent = state_pattern.AgentState()
+        state = getattr(state_pattern, target_state)()
+        agent.set_state(state)
+        return agent.calculate_commission(rooms)
 
 
-if __name__ == "__main__":
+class DiscountFactory:
     """
-    The client code can work with any concrete factory class.
+    Factory class for calculating discount on rental properties.
     """
-    print("Client: Testing client code with the first factory type:")
-    client_code(ConcreteFactory1())
+    def __init__(self):
+        pass
 
-    print("\n")
-
-    print("Client: Testing the same client code with the second factory type:")
-    client_code(ConcreteFactory2())
+    @staticmethod
+    def add_discount(actual_price, age):
+        """
+        Method for selecting the concrete function for calculating discount.
+        """
+        level = "OldAge"
+        if 10 < age < 25:
+            level = "IntermediateAge"
+        target_state = level + "Room"
+        discount = state_pattern.DiscountState()
+        state = getattr(state_pattern, target_state)()
+        discount.set_state(state)
+        return discount.calculate_discount(actual_price)
